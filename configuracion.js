1d01c0761519f64ac093c4aff273990df0048817
guzmanopticaweb <guzmanoptica.web@gmail.com>
2026-07-13 14:14:59 -0300

    Create configuracion.js

---
+++ configuracion.js
@@ -0,0 +1,22 @@
+const configuracionMarca = {
+    // ==========================================================================
+    // PALETA DE COLORES 
+    // ==========================================================================
+    colorPrincipal: "#bf2222",    // Color de los títulos principales y botones activos
+    colorFondoGeneral: "#e5e5e5", // Fondo general de toda la página web
+    colorFondoCeldas: "#e5e5e5",  // Fondo del cuadro donde flota cada lente
+    colorBordes: "#bf2222",       // Color de las líneas divisorias finas
+    colorTextoSecundario: "#bf2222", // Color de los subtítulos y números de modelo
+
+    // ==========================================================================
+    // TIPOGRAFÍA
+    // ==========================================================================
+    tipoLetra: "'Montserrat'",
+
+    // ==========================================================================
+    // CANALES OFICIALES DE COMUNICACIÓN
+    // ==========================================================================
+    numeroWhatsApp: "59899105946",
+    usuarioInstagram: "guzmanoptica",
+    enlaceGoogleMaps: "https://maps.app.goo.gl/kcJWpM5vq37tFHbBA"
+};
